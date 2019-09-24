18c1a0fc917b4bcc0bd0d62973649a3de9833bc5
dylancromer <dylanmattc@gmail.com>
2019-09-24 16:04:03 -0400

    Implemented with scipy.quad

--- projector.py
+++ projector.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.integrate as integrate
 import cubature
 
 
@@ -6,39 +7,21 @@ def _first_term_integrand_func(xs, radii, density_func):
     return 4 * density_func(xs) * xs**2 / radii**2
 
 
-def _second_term_integrand_func(thetas, radii, density_func):
+def _second_term_integrand_func_theta(thetas, radii, density_func):
     return 4 * radii * density_func(radii/np.cos(thetas)) / (4*np.sin(thetas) + 3 - np.cos(2*thetas))
 
 
-def _second_term_integrand_func(xs, radii, density_func):
+def _second_term_integrand_func_x(xs, radii, density_func):
     return 2*xs * (2*xs + (radii**2 - 2*xs**2)/(np.sqrt(xs**2 - radii**2))) * density_func(xs)
 
 
 def esd(radii, density_func):
-    first_term = cubature.cubature(
-        func=_first_term_integrand_func,
-        ndim=radii.size,
-        fdim=radii.size,
-        xmin=np.zeros(radii.size),
-        xmax=radii,
-        args=(radii, density_func),
-        adaptive='p',
-        relerr=1e-3,
-        vectorized=True,
+    first_term = np.array(
+        [integrate.quad(_first_term_integrand_func, 0, r, args=(r, density_func))[0] for r in radii]
     )
 
-    second_term = cubature.cubature(
-        func=_second_term_integrand_func,
-        ndim=radii.size,
-        fdim=radii.size,
-        xmin=radii,
-        xmax=1e2*radii,
-        args=(radii, density_func),
-        adaptive='p',
-        vectorized=True,
-        relerr=1e-3,
+    second_term = np.array(
+        [integrate.quad(_second_term_integrand_func_theta, 0, np.pi/2, args=(r, density_func))[0] for r in radii]
     )
-
-    assert False, second_term
 
     return first_term - second_term
--- spec/esd.py
+++ spec/esd.py
@@ -6,14 +6,14 @@ def describe_esd():
 
     def it_projects_a_constant_density_correctly():
         radii = np.linspace(0.1, 10, 10)
-        rho_func = lambda r: np.ones(r.shape)
+        rho_func = lambda r: 1
         esds = projector.esd(radii, rho_func)
 
-        assert np.all(esds == np.zeros(radii.shape))
+        assert np.allclose(esds, np.zeros(radii.shape))
 
     def it_projects_a_simple_power_law_correctly():
         radii = np.linspace(0.1, 10, 10)
         rho_func = lambda r: 1/r
         esds = projector.esd(radii, rho_func)
 
-        assert np.all(esds == np.ones(radii.shape))
+        assert np.allclose(esds, np.ones(radii.shape))
