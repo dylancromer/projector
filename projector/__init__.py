cfcda94b4f6c900ee93ac5571f86837dce451fc4
dylancromer <dylanmattc@gmail.com>
2019-10-04 10:49:39 -0400

    Switched exclusively to angular integral

--- projector/__init__.py
+++ projector/__init__.py
@@ -1,4 +1,4 @@
-from ._projector import esd, _esd_ang_coords, _esd_los_coords
+from ._projector import esd
 
 
 __all__ = [s for s in dir() if not s.startswith('_')]
--- projector/_projector.py
+++ projector/_projector.py
@@ -10,18 +10,13 @@ def _first_term_integrand_func(xs, radii, density_func):
     return 4 * rhos * postfactor
 
 
-def _second_term_integrand_func_theta(thetas, radii, density_func):
+def _second_term_integrand_func(thetas, radii, density_func):
     radii = radii[:, None]
     thetas = thetas[None, :]
     return 4 * radii * density_func(radii/np.cos(thetas)) / (4*np.sin(thetas) + 3 - np.cos(2*thetas))
 
 
-def _second_term_integrand_func_x(xs, radii, density_func):
-    rs = mathutils.atleast_kd(radii, xs.ndim)
-    return 2*xs * (2*xs + (rs**2 - 2*xs**2)/(np.sqrt(xs**2 - rs**2))) * density_func(xs)
-
-
-def esd(radii, density_func, num_points=100):
+def esd(radii, density_func, num_points=200):
     xs = np.stack(tuple(np.linspace(1e-6, radius, num_points) for radius in radii))
     first_term_integrand = _first_term_integrand_func(xs, radii, density_func)
 
@@ -30,37 +25,7 @@ def esd(radii, density_func, num_points=100):
 
     thetas = np.linspace(0, np.pi/2, num_points)
     dthetas = np.gradient(thetas, axis=-1)
-    second_term_integrand = _second_term_integrand_func_theta(thetas, radii, density_func)
+    second_term_integrand = _second_term_integrand_func(thetas, radii, density_func)
     second_term = mathutils.trapz_(second_term_integrand, axis=-1, dx=dthetas)
-
-    return first_term - second_term
-
-
-def _esd_ang_coords(radii, density_func, num_points=100):
-    xs = np.stack(tuple(np.linspace(1e-6, radius, num_points) for radius in radii))
-    first_term_integrand = _first_term_integrand_func(xs, radii, density_func)
-
-    dxs = mathutils.atleast_kd(np.gradient(xs, axis=-1), first_term_integrand.ndim, append_dims=False)
-    first_term = mathutils.trapz_(first_term_integrand, axis=-1, dx=dxs)
-
-    thetas = np.linspace(0, np.pi/2, num_points)
-    dthetas = np.gradient(thetas, axis=-1).T
-    second_term_integrand = _second_term_integrand_func_theta(thetas, radii, density_func)
-    second_term = mathutils.trapz_(second_term_integrand, axis=-1, dx=dthetas)
-
-    return first_term - second_term
-
-
-def _esd_los_coords(radii, density_func, num_points=100):
-    xs = np.stack(tuple(np.linspace(1e-6, radius, num_points) for radius in radii))
-    first_term_integrand = _first_term_integrand_func(xs, radii, density_func)
-
-    dxs = mathutils.atleast_kd(np.gradient(xs, axis=-1), first_term_integrand.ndim, append_dims=False)
-    first_term = mathutils.trapz_(first_term_integrand, axis=-1, dx=dxs)
-
-    ys = np.stack(tuple(np.linspace(radius+1e-8, 1e2*radius, num_points) for radius in radii))
-    dys = mathutils.atleast_kd(np.gradient(ys, axis=-1), first_term_integrand.ndim, append_dims=False)
-    second_term_integrand = _second_term_integrand_func_x(ys, radii, density_func)
-    second_term = mathutils.trapz_(second_term_integrand, axis=-1, dx=dys)
 
     return first_term - second_term
