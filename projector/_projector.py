2e292b86fb28e967785df11c88add3dfd6e2b3ee
Dylan Cromer <dylan@dylancromer.com>
2020-10-26 20:23:50 -0400

    MAINT: Refactored all functions into container classes

--- projector/_projector.py
+++ projector/_projector.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import numpy as np
 import scipy.integrate as integrate
 import projector.mathutils as mathutils
@@ -23,108 +24,199 @@ def _check_errors_ok(error):
         )
 
 
-def _sd_integrand_func(thetas, radii, density_func, radial_axis_to_broadcast):
-    radii_ = mathutils.atleast_kd(radii, radii.ndim+thetas.ndim)
-    thetas_ = mathutils.atleast_kd(thetas, radii.ndim+thetas.ndim, append_dims=False)
-    if radial_axis_to_broadcast is not None:
-        radii_ = np.moveaxis(radii_, radial_axis_to_broadcast, radii_.ndim-1)
-        thetas_ = np.moveaxis(thetas_, radial_axis_to_broadcast, thetas_.ndim-1)
-    density_arg = radii_/np.cos(thetas_)
-    rhos = density_func(density_arg)
-    radii__ = mathutils.atleast_kd(radii_, rhos.ndim)
-    thetas__ = mathutils.atleast_kd(thetas_, rhos.ndim)
-    return 2 * radii__ * rhos/(np.cos(thetas__)**2)
+@dataclass
+class SurfaceDensity:
+    radii: np.ndarray
+    density_func: object
+    num_points: int
+    radial_axis_to_broadcast: object
+    density_axis: object
+
+    def _pad_radii_and_thetas_for_argument(self, thetas, radii):
+        radii_ = radii.reshape(radii.shape + thetas.ndim*(1,))
+        thetas_ = thetas.reshape(radii.ndim*(1,) + thetas.shape)
+        if self.radial_axis_to_broadcast is not None:
+            radii_ = np.moveaxis(radii_, self.radial_axis_to_broadcast, -1)
+            thetas_ = np.moveaxis(thetas_, self.radial_axis_to_broadcast, -1)
+        return thetas_, radii_
+
+    def _pad_radii_and_thetas_for_integrand(self, thetas, radii, rhos):
+        radii_ = radii.reshape(radii.shape + thetas.ndim*(1,))
+        thetas_ = thetas.reshape(radii.ndim*(1,) + thetas.shape)
+        radii__ = mathutils.atleast_kd(radii_, rhos.ndim)
+        thetas__ = mathutils.atleast_kd(thetas_, rhos.ndim)
+        if self.radial_axis_to_broadcast is not None:
+            radii__ = np.moveaxis(radii__, self.radial_axis_to_broadcast, self.density_axis)
+            thetas__ = np.moveaxis(thetas__, self.radial_axis_to_broadcast, self.density_axis)
+        return thetas__, radii__
+
+    def _sd_integrand_func(self, thetas):
+        thetas_, radii_ = self._pad_radii_and_thetas_for_argument(thetas, self.radii)
+        density_arg = radii_/np.cos(thetas_)
+        rhos = self.density_func(density_arg)
+        thetas__, radii__ = self._pad_radii_and_thetas_for_integrand(thetas, self.radii, rhos)
+        return 2 * radii__ * rhos/(np.cos(thetas__)**2)
+
+    def sd(self):
+        thetas = np.linspace(0, np.pi/2, self.num_points)
+        dthetas = np.gradient(thetas, axis=0)
+        integrand = self._sd_integrand_func(thetas)
+        return mathutils.trapz_(integrand, axis=1, dx=dthetas)
+
+    @classmethod
+    def calculate(cls, radii, density_func, num_points=120, radial_axis_to_broadcast=None, density_axis=-1):
+        return cls(
+            radii=radii,
+            density_func=density_func,
+            num_points=num_points,
+            radial_axis_to_broadcast=radial_axis_to_broadcast,
+            density_axis=density_axis,
+        ).sd()
 
 
-def sd(radii, density_func, num_points=120, radial_axis_to_broadcast=None):
-    thetas = np.linspace(0, np.pi/2, num_points)
-    dthetas = np.gradient(thetas, axis=0)
-    integrand = _sd_integrand_func(thetas, radii, density_func, radial_axis_to_broadcast)
-    return mathutils.trapz_(integrand, axis=1, dx=dthetas)
+@dataclass
+class SurfaceDensity2:
+    radii: np.ndarray
+    density_func: object
+    num_points: int
+    radial_axis_to_broadcast: object
+
+    def _pad_radii_and_ells_for_argument(self, ells, radii):
+        radii_ = mathutils.atleast_kd(radii, radii.ndim+ells.ndim)
+        ells_ = mathutils.atleast_kd(ells, radii.ndim+ells.ndim, append_dims=False)
+        if self.radial_axis_to_broadcast is not None:
+            radii_ = np.moveaxis(radii_, self.radial_axis_to_broadcast, radii_.ndim-1)
+            ells_ = np.moveaxis(ells_, self.radial_axis_to_broadcast, ells_.ndim-1)
+        return ells_, radii_
+
+    def _sd_alt_integrand_func(self, ells):
+        ells_, radii_ = self._pad_radii_and_ells_for_argument(ells, self.radii)
+        density_arg = np.sqrt(radii_**2 + ells_**2)
+        rhos = self.density_func(density_arg)
+        return 2*rhos
+
+    def sd(self):
+        ells = np.geomspace(MIN_INTEGRATION_RADIUS, MAX_INTEGRATION_RADIUS, self.num_points)
+        d_ells = np.gradient(ells, axis=0)
+        integrand = self._sd_alt_integrand_func(ells)
+        return mathutils.trapz_(integrand, axis=1, dx=d_ells)
+
+    @classmethod
+    def calculate(cls, radii, density_func, num_points=120, radial_axis_to_broadcast=None, density_axis=None):
+        return cls(
+            radii=radii,
+            density_func=density_func,
+            num_points=num_points,
+            radial_axis_to_broadcast=radial_axis_to_broadcast,
+        ).sd()
 
 
-def _sd_alt_integrand_func(ells, radii, density_func, radial_axis_to_broadcast):
-    radii_ = mathutils.atleast_kd(radii, radii.ndim+ells.ndim)
-    ells_ = mathutils.atleast_kd(ells, radii.ndim+ells.ndim, append_dims=False)
-    if radial_axis_to_broadcast is not None:
-        radii_ = np.moveaxis(radii_, radial_axis_to_broadcast, radii_.ndim-1)
-        ells_ = np.moveaxis(ells_, radial_axis_to_broadcast, ells_.ndim-1)
-    density_arg = np.sqrt(radii_**2 + ells_**2)
-    rhos = density_func(density_arg)
-    radii__ = mathutils.atleast_kd(radii_, rhos.ndim)
-    ells__ = mathutils.atleast_kd(ells_, rhos.ndim)
-    return 2*rhos
+@dataclass
+class ExcessSurfaceDensity:
+    radii: np.ndarray
+    density_func: object
+    num_points: int
+    radial_axis_to_broadcast: object
+    density_axis: object
+
+    def _esd_first_term_integrand_func(self, xs):
+        rhos = self.density_func(xs)
+        radii_ = self.radii
+        if self.radial_axis_to_broadcast is not None:
+            radii_ = np.moveaxis(radii_, self.radial_axis_to_broadcast, self.radii.ndim-1)
+        postfactor = xs**2 / mathutils.atleast_kd(radii_, xs.ndim, append_dims=False)**2
+        postfactor = mathutils.atleast_kd(postfactor, rhos.ndim)
+        return 4 * rhos * postfactor
+
+    def _esd_second_term_integrand_func(self, thetas):
+        thetas_ = mathutils.atleast_kd(thetas, self.radii.ndim+1)
+        density_arg = self.radii[None, ...]/np.cos(thetas_)
+        rhos = self.density_func(density_arg)
+        radii = mathutils.atleast_kd(self.radii[None, ...], rhos.ndim)
+        thetas = mathutils.atleast_kd(thetas_, rhos.ndim)
+        return 4 * radii * rhos / (4*np.sin(thetas) + 3 - np.cos(2*thetas))
+
+    def esd(self):
+        xs = np.linspace(MIN_INTEGRATION_RADIUS, self.radii, self.num_points)
+        first_term_integrand = self._esd_first_term_integrand_func(xs)
+
+        dxs = mathutils.atleast_kd(np.gradient(xs, axis=0), first_term_integrand.ndim)
+        first_term = mathutils.trapz_(first_term_integrand, axis=0, dx=dxs)
+
+        thetas = np.linspace(0, np.pi/2, self.num_points)
+        dthetas = np.gradient(thetas, axis=0)
+        second_term_integrand = self._esd_second_term_integrand_func(thetas)
+        second_term = mathutils.trapz_(second_term_integrand, axis=0, dx=dthetas)
+
+        return first_term - second_term
+
+    @classmethod
+    def calculate(cls, radii, density_func, num_points=120, radial_axis_to_broadcast=None, density_axis=None):
+        return cls(
+            radii=radii,
+            density_func=density_func,
+            num_points=num_points,
+            radial_axis_to_broadcast=radial_axis_to_broadcast,
+            density_axis=density_axis,
+        ).esd()
 
 
-def sd_alt(radii, density_func, num_points=120, radial_axis_to_broadcast=None):
-    ells = np.geomspace(MIN_INTEGRATION_RADIUS, MAX_INTEGRATION_RADIUS, num_points)
-    d_ells = np.gradient(ells, axis=0)
-    integrand = _sd_alt_integrand_func(ells, radii, density_func, radial_axis_to_broadcast)
-    return mathutils.trapz_(integrand, axis=1, dx=d_ells)
+@dataclass
+class QuadExcessSurfaceDensity:
+    radii: np.ndarray
+    density_func: object
+    num_points: int
+    radial_axis_to_broadcast: object
+    density_axis: object
 
+    def _esd_first_term_integrand_func(self, xs, radius):
+        rhos = self.density_func(xs)
+        postfactor = xs**2 / mathutils.atleast_kd(radius, xs.ndim, append_dims=False)**2
+        postfactor = mathutils.atleast_kd(postfactor, rhos.ndim)
+        return 4 * rhos * postfactor
 
-def _esd_first_term_integrand_func(xs, radii, density_func, radial_axis_to_broadcast):
-    rhos = density_func(xs)
-    radii_ = radii
-    if radial_axis_to_broadcast is not None:
-        radii_ = np.moveaxis(radii_, radial_axis_to_broadcast, radii_.ndim-1)
-    postfactor = xs**2 / mathutils.atleast_kd(radii_, xs.ndim, append_dims=False)**2
-    postfactor = mathutils.atleast_kd(postfactor, rhos.ndim)
-    return 4 * rhos * postfactor
+    def _esd_second_term_integrand_func(self, thetas):
+        thetas_ = mathutils.atleast_kd(thetas, self.radii.ndim+1)
+        density_arg = self.radii[None, ...]/np.cos(thetas_)
+        rhos = self.density_func(density_arg)
+        radii = mathutils.atleast_kd(self.radii[None, ...], rhos.ndim)
+        thetas = mathutils.atleast_kd(thetas_, rhos.ndim)
+        return 4 * radii * rhos / (4*np.sin(thetas) + 3 - np.cos(2*thetas))
 
+    def _integrate_esd_first_term_quad(self):
+        rflats = self.radii.flatten()
+        first_term_integral = np.array(
+            [integrate.quad_vec(
+                lambda x: self._esd_first_term_integrand_func(np.array([x]), rflats[i:i+1]),
+                MIN_INTEGRATION_RADIUS,
+                rflats[i],
+            ) for i in range(self.radii.size)],
+            dtype=object,
+        )
+        _check_errors_ok(first_term_integral[:, 1].astype(float))
+        return np.concatenate(first_term_integral[:, 0]).astype(float)
 
-def _esd_second_term_integrand_func(thetas, radii, density_func):
-    thetas_ = mathutils.atleast_kd(thetas, radii.ndim+1)
-    density_arg = radii[None, ...]/np.cos(thetas_)
-    rhos = density_func(density_arg)
-    radii = mathutils.atleast_kd(radii[None, ...], rhos.ndim)
-    thetas = mathutils.atleast_kd(thetas_, rhos.ndim)
-    return 4 * radii * rhos / (4*np.sin(thetas) + 3 - np.cos(2*thetas))
+    def _integrate_esd_second_term_quad(self):
+        second_term, second_term_errors = integrate.quad_vec(
+            lambda theta: self._esd_second_term_integrand_func(np.array([theta])),
+            0,
+            np.pi/2,
+        )
+        _check_errors_ok(second_term_errors)
+        return second_term
 
+    def esd(self):
+        dens_shape = self.density_func(self.radii).shape
+        first_term = self._integrate_esd_first_term_quad().reshape(dens_shape)
+        second_term = self._integrate_esd_second_term_quad().reshape(dens_shape)
+        return first_term - second_term
 
-def esd(radii, density_func, num_points=120, radial_axis_to_broadcast=None):
-    xs = np.linspace(MIN_INTEGRATION_RADIUS, radii, num_points)
-    first_term_integrand = _esd_first_term_integrand_func(xs, radii, density_func, radial_axis_to_broadcast)
-
-    dxs = mathutils.atleast_kd(np.gradient(xs, axis=0), first_term_integrand.ndim)
-    first_term = mathutils.trapz_(first_term_integrand, axis=0, dx=dxs)
-
-    thetas = np.linspace(0, np.pi/2, num_points)
-    dthetas = np.gradient(thetas, axis=0)
-    second_term_integrand = _esd_second_term_integrand_func(thetas, radii, density_func)
-    second_term = mathutils.trapz_(second_term_integrand, axis=0, dx=dthetas)
-
-    return first_term - second_term
-
-
-def _integrate_esd_first_term_quad(radii, density_func):
-    rflats = radii.flatten()
-    first_term_integral = np.array(
-        [integrate.quad_vec(
-            lambda x: _esd_first_term_integrand_func(np.array([x]), rflats[i:i+1], density_func, radial_axis_to_broadcast=None),
-            MIN_INTEGRATION_RADIUS,
-            rflats[i],
-        ) for i in range(radii.size)],
-        dtype=object,
-    )
-    _check_errors_ok(first_term_integral[:, 1].astype(float))
-    return np.concatenate(first_term_integral[:, 0]).astype(float)
-
-
-def _integrate_esd_second_term_quad(radii, density_func):
-    second_term, second_term_errors = integrate.quad_vec(
-        lambda theta: _esd_second_term_integrand_func(np.array([theta]), radii, density_func),
-        0,
-        np.pi/2,
-    )
-    _check_errors_ok(second_term_errors)
-    return second_term
-
-
-def esd_quad(radii, density_func):
-    dens_shape = density_func(radii).shape
-    first_term = _integrate_esd_first_term_quad(radii, density_func).reshape(dens_shape)
-    second_term = _integrate_esd_second_term_quad(radii, density_func).reshape(dens_shape)
-    return first_term - second_term
-
-
+    @classmethod
+    def calculate(cls, radii, density_func, num_points=120, radial_axis_to_broadcast=None, density_axis=None):
+        return cls(
+            radii=radii,
+            density_func=density_func,
+            num_points=num_points,
+            radial_axis_to_broadcast=radial_axis_to_broadcast,
+            density_axis=density_axis,
+        ).esd()
--- spec/projector_spec.py
+++ spec/projector_spec.py
@@ -3,104 +3,105 @@ import numpy as np
 import projector
 
 
-def describe_sd():
+def describe_SurfaceDensity():
 
     def it_projects_an_inverse_square_law_correctly():
         radii = np.geomspace(0.1, 10, 30)
         rho_func = lambda r: 1/r**2
-        sds = projector.sd(radii, rho_func)
+        sds = projector.SurfaceDensity.calculate(radii, rho_func)
 
         assert np.allclose(sds, np.pi/radii, rtol=1e-4)
 
     def it_can_handle_larger_shapes():
         radii = np.linspace(0.1, 10, 10)
         rho_func = lambda r: projector.mathutils.atleast_kd(1/r, r.ndim + 3) * np.random.random_sample(r.shape + (3, 4, 5))
-        sds = projector.sd(radii, rho_func)
+        sds = projector.SurfaceDensity.calculate(radii, rho_func)
 
         assert not np.any(np.isnan(sds))
 
     def it_can_allow_radii_that_are_functions_of_other_parameters():
         def rho_func(r, z):
             z_ = projector.mathutils.atleast_kd(z, r.ndim, append_dims=False)
-            return z_/r**2
+            return (z_/r**2)[..., None]
 
         zs = np.linspace(1, 2, 3)
         rs = np.array([
             np.linspace(i, i+1, 10) for i in range(1, zs.size+1)
         ]).T
-        sds = projector.sd(rs, lambda r: rho_func(r, zs), radial_axis_to_broadcast=1)
+        sds = projector.SurfaceDensity.calculate(rs, lambda r: rho_func(r, zs), radial_axis_to_broadcast=1, density_axis=-2)
 
         zs = projector.mathutils.atleast_kd(zs, rs.ndim, append_dims=False)
 
-        assert np.allclose(sds, zs * np.pi/rs, rtol=1e-4)
+        assert np.allclose(sds, (zs * np.pi/rs)[..., None], rtol=1e-4)
 
 
-def describe_sd_alt():
+def describe_SurfaceDensity2():
 
     def it_projects_an_inverse_square_law_correctly():
         radii = np.geomspace(0.1, 10, 30)
         rho_func = lambda r: 1/r**2
-        sds = projector.sd_alt(radii, rho_func)
+        sds = projector.SurfaceDensity2.calculate(radii, rho_func)
 
         assert np.allclose(sds, np.pi/radii, rtol=1e-2)
 
     def it_can_handle_larger_shapes():
         radii = np.linspace(0.1, 10, 10)
         rho_func = lambda r: projector.mathutils.atleast_kd(1/r, r.ndim + 3) * np.random.random_sample(r.shape + (3, 4, 5))
-        sds = projector.sd_alt(radii, rho_func)
+        sds = projector.SurfaceDensity2.calculate(radii, rho_func)
 
         assert not np.any(np.isnan(sds))
 
     def it_can_allow_radii_that_are_functions_of_other_parameters():
         def rho_func(r, z):
             z_ = projector.mathutils.atleast_kd(z, r.ndim, append_dims=False)
-            return z_/r**2
+            return (z_/r**2)[..., None]
 
         zs = np.linspace(1, 2, 3)
         rs = np.array([
             np.linspace(i, i+1, 10) for i in range(1, zs.size+1)
         ]).T
-        sds = projector.sd_alt(rs, lambda r: rho_func(r, zs), radial_axis_to_broadcast=1)
+        sds = projector.SurfaceDensity2.calculate(rs, lambda r: rho_func(r, zs), radial_axis_to_broadcast=1, density_axis=-2)
 
         zs = projector.mathutils.atleast_kd(zs, rs.ndim, append_dims=False)
 
-        assert np.allclose(sds, zs * np.pi/rs, rtol=1e-2)
+        assert np.allclose(sds, (zs * np.pi/rs)[..., None], rtol=1e-2)
 
 
-def describe_esd():
+
+def describe_ExcessSurfaceDensity():
 
     def it_projects_a_constant_density_correctly():
         radii = np.linspace(0.1, 10, 10)
         rho_func = lambda r: np.ones(r.shape)
-        esds = projector.esd(radii, rho_func)
+        esds = projector.ExcessSurfaceDensity.calculate(radii, rho_func)
 
         assert np.allclose(esds, np.zeros(radii.shape), atol=1e-3)
 
     def it_projects_a_simple_power_law_correctly():
         radii = np.linspace(0.1, 10, 10)
         rho_func = lambda r: 1/r
-        esds = projector.esd(radii, rho_func)
+        esds = projector.ExcessSurfaceDensity.calculate(radii, rho_func)
 
         assert np.allclose(esds, np.ones(radii.shape), rtol=1e-4)
 
     def it_can_handle_larger_shapes():
         radii = np.linspace(0.1, 10, 10)
         rho_func = lambda r: projector.mathutils.atleast_kd(1/r, r.ndim + 3) * np.random.random_sample(r.shape + (3, 4, 5))
-        esds = projector.esd(radii, rho_func)
+        esds = projector.ExcessSurfaceDensity.calculate(radii, rho_func)
 
         assert not np.any(np.isnan(esds))
 
     def it_projects_an_inverse_square_law_correctly():
         radii = np.linspace(0.1, 10, 30)
         rho_func = lambda r: 1/r**2
-        esds = projector.esd(radii, rho_func)
+        esds = projector.ExcessSurfaceDensity.calculate(radii, rho_func)
 
         assert np.allclose(esds, np.pi/radii, rtol=1e-4)
 
     def it_projects_a_gaussian_correctly():
         radii = np.logspace(-2, 1, 30)
         rho_func = lambda r: np.exp(-r**2 / 2)
-        esds = projector.esd(radii, rho_func, num_points=300)
+        esds = projector.ExcessSurfaceDensity.calculate(radii, rho_func, num_points=300)
 
         pref = np.sqrt(2 * np.pi)
         true = pref * (2/radii**2 - np.exp(-radii**2/2) * (1 + 2/radii**2))
@@ -109,53 +110,53 @@ def describe_esd():
     def it_can_allow_radii_that_are_functions_of_other_parameters():
         def rho_func(r, z):
             z_ = projector.mathutils.atleast_kd(z, r.ndim, append_dims=False)
-            return z_/r**2
+            return (z_/r**2)[..., None]
 
         zs = np.linspace(1, 2, 3)
         rs = np.array([
             np.linspace(i, i+1, 10) for i in range(1, zs.size+1)
         ]).T
-        sds = projector.esd(rs, lambda r: rho_func(r, zs), radial_axis_to_broadcast=1)
+        sds = projector.ExcessSurfaceDensity.calculate(rs, lambda r: rho_func(r, zs), radial_axis_to_broadcast=1)
 
         zs = projector.mathutils.atleast_kd(zs, rs.ndim, append_dims=False)
 
-        assert np.allclose(sds, zs * np.pi/rs, rtol=1e-4)
+        assert np.allclose(sds, (zs * np.pi/rs)[..., None], rtol=1e-4)
 
 
-def describe_esd_quad():
+def describe_QuadExcessSurfaceDensity():
 
     def it_projects_a_constant_density_correctly():
         radii = np.linspace(0.1, 10, 10)
         rho_func = lambda r: np.ones(r.shape)
-        esds = projector.esd_quad(radii, rho_func)
+        esds = projector.QuadExcessSurfaceDensity.calculate(radii, rho_func)
 
         assert np.allclose(esds, np.zeros(radii.shape), atol=1e-3)
 
     def it_can_handle_a_constant_in_2d():
         radii = np.linspace(0.1, 10, 2)
         rho_func = lambda r: np.ones(r.shape + (4,))
-        esds = projector.esd_quad(radii, rho_func)
+        esds = projector.QuadExcessSurfaceDensity.calculate(radii, rho_func)
 
         assert np.allclose(esds, np.zeros(radii.shape + (4,)))
 
     def it_projects_a_simple_power_law_correctly():
         radii = np.linspace(0.1, 10, 10)
         rho_func = lambda r: 1/r
-        esds = projector.esd_quad(radii, rho_func)
+        esds = projector.QuadExcessSurfaceDensity.calculate(radii, rho_func)
 
         assert np.allclose(esds, np.ones(radii.shape), rtol=1e-4)
 
     def it_projects_an_inverse_square_law_correctly():
         radii = np.linspace(0.1, 10, 30)
         rho_func = lambda r: 1/r**2
-        esds = projector.esd_quad(radii, rho_func)
+        esds = projector.QuadExcessSurfaceDensity.calculate(radii, rho_func)
 
         assert np.allclose(esds, np.pi/radii, rtol=1e-4)
 
     def it_can_handle_larger_shapes():
         radii = np.linspace(0.1, 10, 10)
         rho_func = lambda r: projector.mathutils.atleast_kd(1/r, r.ndim + 3)
-        esds = projector.esd_quad(radii, rho_func)
+        esds = projector.QuadExcessSurfaceDensity.calculate(radii, rho_func)
 
         assert np.allclose(esds, np.ones(radii.shape + 3*(1,)), rtol=1e-4)
 
@@ -163,4 +164,4 @@ def describe_esd_quad():
         radii = np.linspace(0.1, 10, 10)
         rho_func = lambda r: np.ones(r.shape) * 1e30
         with pytest.raises(projector.LargeQuadratureErrorsException):
-            projector.esd_quad(radii, rho_func)
+            projector.QuadExcessSurfaceDensity.calculate(radii, rho_func)
